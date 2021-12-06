74975f83ed9a9bba077ed13be144c7b5fff50b4a
axelav <axel@ftml.net>
2021-12-05 23:09:07 -0500

    feat: add script to scrape data from html

---
+++ scripts/scrape.js
@@ -0,0 +1,60 @@
+const { resolve } = require('path')
+const { readdir, readFile, writeFile } = require('fs').promises
+const { JSDOM } = require('jsdom')
+const { DateTime } = require('luxon')
+
+const timestampToISO = str => DateTime.fromFormat(
+  str.trim().replace(/(?<=[0-9])(?:st|nd|rd|th)/, ''),
+  'MMMM d, yyyy h:mma'
+).toISO()
+
+const getFiles = async (dir) => {
+  const dirents = await readdir(dir, { withFileTypes: true })
+  const files = await Promise.all(
+    dirents.map((dirent) => {
+      const res = resolve(dir, dirent.name)
+      return dirent.isDirectory() ? getFiles(res) : res
+    })
+  )
+  return files.flat()
+}
+
+const getFileContents = async (file) => {
+  const contents = await readFile(file)
+
+  return contents.toString()
+}
+
+const getPost = (contents) => {
+  const dom = new JSDOM(contents)
+
+  const imgSrc = dom.window.document.querySelector('img').src
+  const caption = dom.window.document.querySelector('.caption')?.textContent
+  const timestamp = dom.window.document.querySelector('#timestamp').textContent
+
+  return {
+    imgSrc: imgSrc.replace(/^\.\.\/\./, ''),
+    caption,
+    timestamp: timestampToISO(timestamp),
+  }
+}
+
+const main = async () => {
+  const files = await getFiles(__dirname + '/../posts/html')
+
+  const data = await Promise.all(
+    files.map(async (file) => {
+      const contents = await getFileContents(file)
+
+      return getPost(contents)
+    })
+  )
+
+  files.sort((a, b) =>
+    a.timestamp < b.timestamp ? -1 : a.timestamp > b.timestamp ? 1 : 0
+  )
+
+  await writeFile(__dirname + '/../data.json', JSON.stringify(data))
+}
+
+main()
